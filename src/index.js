91373975e34d39fbaf0aa074c94653591c07a8f4
limdaeun1 <limdaeun1_@naver.com>
2022-09-09 14:11:25 +0900

    gitignore 생성

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { Store } from "./redux/config/store";
+import store from "./redux/config/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
