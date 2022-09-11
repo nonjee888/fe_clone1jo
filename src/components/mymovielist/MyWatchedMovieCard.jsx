d5b56a227cc5ece8d8f3d1fcf9a9b4414dfd4aca
Eunji Roh <nonjee888@gmail.com>
2022-09-11 23:14:16 +0900

    MyWatchedList 완성

--- src/components/mymovielist/MyWatchedMovieCard.jsx
+++ src/components/mymovielist/MyWatchedMovieCard.jsx
@@ -4,7 +4,33 @@ import styled from "styled-components";
 const MyWatchedMovieCard = () => {
   return (
     <div>
-      <MovieInfoLi></MovieInfoLi>
+      <MovieInfoLi>
+        <MovieInfo>
+          <BoxImage>
+            <A
+              title="포스터 크게보기"
+              href="" //링크 주소 넣을곳
+            >
+              <Span>포스터 나오는곳</Span>
+            </A>
+          </BoxImage>
+          <BoxContent>
+            <DeleteBtn></DeleteBtn>
+            <BoxTitle>
+              <A
+                title="디테일 페이지"
+                href="" //링크 주소 넣을곳
+              >
+                <Strong id="movietitle">타이틀</Strong>
+              </A>
+              <P>영어이름</P>
+              <Date>2015.00.00 (일) 00:00 ~ 00:00</Date>
+              <Place>CGV 강남 3관 8층 / 2명</Place>
+              <Rate href="">이 영화를 평가해주세요</Rate>
+            </BoxTitle>
+          </BoxContent>
+        </MovieInfo>
+      </MovieInfoLi>
     </div>
   );
 };
@@ -21,11 +47,76 @@ const MovieInfoLi = styled.li`
   height: 158px;
   border: 1px solid green;
 `;
+const MovieInfo = styled.div`
+  position: relative;
+  width: 720px;
+  height: 158px;
+`;
+const BoxImage = styled.div`
+  float: left;
+  width: 110px;
+  height: 158px;
+  margin-right: 22px;
+`;
+const A = styled.a``;
+const Span = styled.span`
+  width: 110px;
+  height: 154.59px;
+  display: block;
+  position: relative;
+`;
+const BoxContent = styled.div`
+  position: relative;
+  width: 550px;
+  height: 139px;
+  float: left;
+`;
+const Strong = styled.strong``;
+const BoxTitle = styled.div`
+  font-size: 23px;
+  width: 550px;
+  height: 45px;
+  margin: 14px 0;
+`;
+const P = styled.p`
+  color: #666;
+  font-family: verdana, sans-serif;
+  font-size: 11px;
+  font-weight: bold;
+`;
 const DeleteBtn = styled.button`
+  position: absolute;
+  bottom: 100px;
+  left: 540px;
   width: 25px;
   height: 25px;
   border-radius: 50%;
-  background: black;
   border: none;
-  background-img: url("https://img.cgv.co.kr/R2014/images/common/btn/btn_del.gif");
+  background: url("https://img.cgv.co.kr/R2014/images/common/btn/btn_del.gif");
+`;
+const Date = styled.p`
+  margin-top: 15px;
+  color: #222;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+  margin-bottom: 9px;
+`;
+const Place = styled.p`
+  color: #222;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+  margin-bottom: 15px;
+`;
+const Rate = styled.a`
+  display: block;
+  font-weight: 500;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  color: inherit;
+  text-decoration: none;
+  font-size: 12px;
+  font-weight: 850;
 `;
--- src/pages/MyMovieList.jsx
+++ src/pages/MyMovieList.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Headers from "../components/layout/Headers";
+import Footers from "../components/layout/Footers";
 import MyWatchedMovie from "../components/mymovielist/MyWatchedMovie";
 
 function MyMovieList() {
   return (
     <div>
+      <Headers />
       <MyWatchedMovie />
+      <Footers />
     </div>
   );
 }
