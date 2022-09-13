02268984b52503bdabb4a6b346edce4d602cb3c7
limdaeun1 <limdaeun1_@naver.com>
2022-09-13 22:59:50 +0900

    예매하기 구현중 / 좌석선택 미구현

--- src/components/bookmovie/BookMovie.jsx
+++ src/components/bookmovie/BookMovie.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { React, useState ,useEffect} from "react";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { choiceMovie } from "../../redux/modules/booking";
 
 function BookingTime() {
+  
+  const [movieTitle, setMovieTitle] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(choiceMovie(movieTitle))
+  }, [movieTitle]);
 
   return (
     <>
@@ -9,20 +18,20 @@ function BookingTime() {
 
     <Tabmenu>
       <Tab1>
-    <span class="side on"></span>
-    <a href="#" class="button menu1 selected">
+    <span></span>
+    <a href="#">
       전체</a>
       </Tab1>
 
       <Tab>
-    <span class="side on"></span>
-    <a href="http://www.cgv.co.kr/arthouse/" class="button menu2">
+    <span></span>
+    <a href="http://www.cgv.co.kr/arthouse/">
       아트하우스▼</a>
     </Tab>
 
       <Tab>
-      <span class="arrow"></span>
-      <a href="http://www.cgv.co.kr/theaters/special/defaultNew.aspx" class="button menu3">
+      <span></span>
+      <a href="http://www.cgv.co.kr/theaters/special/defaultNew.aspx">
         특별관▼</a>
         </Tab>
 
@@ -31,118 +40,38 @@ function BookingTime() {
     <Sortmenu>
 
       <Tab2>
-      <a href="#" onclick="sortMovieByRank();return false;" 
-      id="movieSortRankBtn" class="button btn-rank selected">예매율순</a>
+      <a href="#" 
+      id="movieSortRankBtn">예매율순</a>
       </Tab2>
 
       <Tab2>
-      <a href="#" onclick="sortMovieByName();return false;" 
-      id="movieSortNameBtn" class="button btn-abc">가나다순</a>
+      <a href="#" 
+      id="movieSortNameBtn">가나다순</a>
       </Tab2>
     </Sortmenu>
-
+    {/* onclick={setMovieTitle("공조2-인터네셔날")} */}
     <Movielist>
       
-      <li>
-        <a href="#" onclick="return false;" title="알라딘" alt="알라딘">
-          <span>알라딘</span>
-          </a></li>
+            <li>
+            <a href="#" onClick={()=>setMovieTitle("공조2-인터내셔날")}>
+            <span>공조2-인터내셔날</span>
+            </a></li>
 
-          <li>
-            <a href="#" onclick="return false;" title="탑건-매버릭" alt="탑건-매버릭">
-            <span>탑건-매버릭</span>
+           <li>
+            <a href="#" onClick={()=>setMovieTitle("육사오")}>
+            <span>육사오</span>
             </a></li>
 
             <li>
-            <a href="#" onclick="return false;" title="헌트" alt="헌트">
-            <span class="text">헌트</span>
+            <a href="#" onClick={()=>setMovieTitle("알라딘")}>
+            <span>알라딘</span>
             </a></li>
 
             <li>
-            <a href="#" onclick="return false;" title="한산: 용의 출현" alt="한산: 용의 출현">
-            <span class="text">한산: 용의 출현</span>
+            <a href="#" onClick={()=>setMovieTitle("탑건-메버릭")}>
+            <span>탑건-메버릭</span>
             </a></li>
 
-            <li>
-            <a href="#" onclick="return false;" title="놉" alt="놉">
-            <span class="text">놉</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="사랑할 땐 누구나 최악이 된다" alt="사랑할 땐 누구나 최악이 된다">
-            <span class="text">사랑할 땐 누구나 최악이 된다</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="시맨틱 에러: 더 무비" alt="시맨틱 에러: 더 무비">
-            <span class="text">시맨틱 에러: 더 무비</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="한 여름밤의 재즈" alt="한 여름밤의 재즈">
-            <span class="text">한 여름밤의 재즈</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="헤어질 결심" alt="헤어질 결심">
-            <span class="text">헤어질 결심</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="미니언즈2" alt="미니언즈">
-            <span class="text">미니언즈</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="불릿 트레인" alt="불릿 트레인">
-            <span class="text">불릿 트레인</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="노스맨" alt="노스맨">
-            <span class="text">노스맨</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="토르: 러브 앤 썬더" alt="토르: 러브 앤 썬더">
-            <span class="text">토르: 러브 앤 썬더</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="엘비스" alt="엘비스">
-            <span class="text">엘비스</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="그레이 맨" alt="그레이 맨">
-            <span class="text">그레이 맨</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="마녀(魔女) Part2. The Other One" alt="마녀(魔女) Part2. The Other One">
-            <span class="text">마녀(魔女) Part2. The Other One</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="모가디슈" alt="모가디슈">
-            <span class="text">모가디슈</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="닥터 스트레인지: 대혼돈의 멀티버스" alt="닥터 스트레인지: 대혼돈의 멀티버스">
-            <span class="text">닥터 스트레인지: 대혼돈의 멀티버스</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="더 배트맨" alt="더 배트맨">
-            <span class="text">더 배트맨</span>
-            </a></li>
-
-            <li>
-            <a href="#" onclick="return false;" title="신비한 동물들과 덤블도어의 비밀" alt="신비한 동물들과 덤블도어의 비밀">
-            <span class="text">신비한 동물들과 덤블도어의 비밀</span>
-            </a></li>
-            
     </Movielist>
     </>
   )
--- src/pages/BookMovie.jsx
+++ src/pages/BookMovie.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Headers from "../components/layout/Headers";
 import BookMovieLayout from "../components/bookmovie/BookMovieLayout";
+import Footers from '../components/layout/Footers'
 
 function BookMovie() {
   return (<div>
-    <Headers/ >
-    <BookMovieLayout/ >  
+    <Headers/>
+    <BookMovieLayout/>
+    <Footers/>
   </div>
   );
 }
